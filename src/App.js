7e8cc0df205d6d9731d626692c6679439a5f0cc7
SamchanThunder <samchan.lee@gmail.com>
2024-06-27 14:13:53 +0900

    Update

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="calendarBackground">
+      <div id="topCalendar">
+        <button id="leftArrow">&lt;</button>
+        <div id="monthText">Text</div>
+        <button id="rightArrow">&gt;</button>
+      </div>
+      <div id="bottomCalendar">
+        <div id="rowCalendar">
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+        </div>
+
+        <div id="rowCalendar">
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+        </div>
+        <div id="rowCalendar">
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+        </div>
+        <div id="rowCalendar">
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+        </div>
+        <div id="rowCalendar">
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+          <button className="date">1</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
